8acefd999372af15b680525a3971bb30cb853d36
textfield <textfield@32d87bda-894e-0410-9cde-b4b2fc69aec3>
2014-02-25 12:13:22 +0000

    Exit.

--- src.d/maddog_backup_delete.sh
+++ src.d/maddog_backup_delete.sh
@@ -6,3 +6,4 @@
 # Remove empty directories.
 
 /usr/bin/find /var/www/maddog-backup.d -depth -type d -name "*.d" -empty -exec /bin/rmdir {} \;
+exit 0
--- src.d/maddog_backup_tmp_removal.sh
+++ src.d/maddog_backup_tmp_removal.sh
@@ -4,3 +4,4 @@
 # If a file is older than 24 hours, remove it.
 
 /usr/bin/find /var/www/maddog-backup.d/data -mtime +1 -name "bcksys.*" -exec /bin/rm {} \;
+exit 0
